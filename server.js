098612635427c0ccdd4eb53d6c001d48ca89d8f7
agauniyal <agauniyal@outlook.com>
2015-03-20 15:34:57 +0530

    Added diff console.log

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var counter = 0;
 http.createServer(function (req, res) {
   res.writeHead(200);
   counter += 1;
+  console.log("I'm inside server loop");
   res.end("hello world : "+ counter + "\n" );
 }).listen(8080);
 
+console.log("I'm outside server loop");
